34d2ba41c916e0da3b820f1f9971d9763885ed41
beatrizacbs <beatrizacbs@gmail.com>
2017-07-25 11:14:55 -0300

    2

--- HOPE.sql
+++ HOPE.sql
@@ -18,52 +18,67 @@ create table doacao(
     
 create table doacao_voluntario(
 	disp_horario varchar(50),
-    id_voluntario varchar(10), -- Aqui eu coloquei uma chave pra identificar e não ficar uma entdade sem chave.
+    id_voluntario varchar(10),
+    data_doacao_voluntario datetime,
     constraint doacao_voluntario_pk primary key(id_voluntario),
-    constraint voluntario_id_voluntario_fk foreign key(id_voluntario) REFERENCES doacao(id_doacao)
+    constraint voluntario_id_voluntario_fk foreign key(id_voluntario) REFERENCES doacao(id_doacao),
+    constraint data_doacao_volutario_fk foreign key (data_doacao_voluntario) references doacao(data_doacao)
 );
     
 create table doacao_financeiro(
 	id_financeiro varchar(10),
+    data_doacao_financeiro datetime,
     constraint doacao_finandeiro_pk primary key(id_financeiro),
-    constraint doacao_financeiro_fk foreign key (id_financeiro) references doacao(id_doacao)
+    constraint doacao_financeiro_fk foreign key (id_financeiro) references doacao(id_doacao),
+    constraint data_doacao_financeiro_fk foreign key (data_doacao_financeiro) references doacao(data_doacao)
 );
 
 create table doacao_produto(
 	id_produto varchar(10),
+    data_doacao_produto datetime,
     constraint doacao_produto_pk primary key(id_produto),
-    constraint doacao_produto_fk foreign key (id_produto) references doacao(id_doacao)
+    constraint doacao_produto_fk foreign key (id_produto) references doacao(id_doacao),
+    constraint data_doacao_produto_fk foreign key (data_doacao_produto) references doacao(data_doacao)
 );
 
-create table alimento(
+create table doacao_alimento(
 	id_alimento varchar(10),
-    validade date, -- Algumas coisas tão sem chave, eu to colocando chave estrangeira, por causa das heranças. 
+    validade date,
+    data_doacao_alimento datetime, 
     tipo_almento varchar(13),
-    constraint alimento_pk primary key(id_alimento),
-    constraint alimento_fk foreign key (id_alimento) references doacao(id_doacao),
-    constraint alimento_tipo_alimento_ck check(tipo_alimento in ('NÃO PERECIVEL', 'PERECIVEL'))
+    constraint doacao_alimento_pk primary key(id_alimento),
+    constraint doacao_alimento_fk foreign key (id_alimento) references doacao(id_doacao),
+    constraint doacao_alimento_tipo_alimento_ck check(tipo_alimento in ('NÃO PERECIVEL', 'PERECIVEL')),
+    constraint data_doacao_alimento_fk foreign key (data_doacao_alimento) references doacao(data_doacao)
     
 ); 
 
-create table vestimenta(
+-- Tem que olhar isso aqui depois pra ver se as entidades relacionadas com ela não dão pra ser atributos
+create table doacao_vestimenta(
 	id_vestimenta varchar(10),
-    constraint vestimenta_pk primary key(id_vestimenta),
-    constraint vestimenta_fk foreign key (id_vestimenta) references doacao(id_doacao)
+    data_doacao_vestimenta datetime,
+    constraint doacao_vestimenta_pk primary key(id_vestimenta),
+    constraint doacao_vestimenta_fk foreign key (id_vestimenta) references doacao(id_doacao),
+    constraint data_doacao_vestimenta_fk foreign key (data_doacao_vestimenta) references doacao(data_doacao)
 );
 
-create table higiene(
+create table doacao_higiene(
 	id_higiene varchar(10),
     tipo_higiene varchar(7),
-    constraint higiene_pk primary key(id_higiene),
-    constraint higiene_fk foreign key (id_higiene) references doacao(id_doacao),
-    constraint higiene_tipo_higiene_ck check(tipo_higiene in ('LIMPEZA', 'PESSOAL'))
+    data_doacao_higiene datetime,
+    constraint doacao_higiene_pk primary key(id_higiene),
+    constraint doacao_higiene_fk foreign key (id_higiene) references doacao(id_doacao),
+    constraint doacao_higiene_tipo_higiene_ck check(tipo_higiene in ('LIMPEZA', 'PESSOAL')),
+    constraint data_doacao_higiene_fk foreign key (data_doacao_higiene) references doacao(data_doacao)
 );
 
-create table diversos(
+create table doacao_diversos(
 	id_diversos varchar(10),
     descricao varchar(140),
-	constraint diversos_pk primary key(id_diversos),
-    constraint diversos_fk foreign key(id_diversos) references doacao(id_doacao)
+    data_doacao_diversos datetime,
+	constraint doacao_diversos_pk primary key(id_diversos),
+    constraint doacao_diversos_fk foreign key(id_diversos) references doacao(id_doacao),
+    constraint data_doacao_diversos_fk foreign key (data_doacao_diversos) references doacao(data_doacao)
 );
 
 create table doador(
@@ -73,10 +88,26 @@ create table doador(
 );
 
 create table doador_juridica(
-	nome varchar(30), 
-    CNPJ varchar(14)
-    
+	id_doador_juridica varchar(10),
+    num_doacoes_dj int,
+    nome varchar(20), 
+    CNPJ varchar(14),   
+	endereco_dj varchar(8), 
+    constraint doador_juridica_pk primary key (id_doador_juridica), 
+    constraint doador_juridica_fk foreign key (id_doador_juridica) references doador(id_doador),
+    constraint endereco_dj_fk foreign key (endereco_dj) references endereco(CEP),
+    constraint num_doacoes_dj_fk foreign key (num_doacoes_dj) references doador(num_doacoes)
+);
 
+create table doador_fisica(
+	num_doacoes_pf int,
+    id_doador_fisica varchar(10),
+    nome varchar(50),
+    CPF varchar(11),
+    endereco_pf varchar(8),
+    constraint id_doador_fisica_pk primary key (id_doador_fisica),
+    constraint id_doador_fisica_fk foreign key (id_doador_fisica) references doador(id_doador),
+    constraint endereco_pf_fk foreign key (endereco_pf) references endereco(CEP)
 );
 
 create table endereco(
