35699f3a3cf64f2902f7264f0d7ac736c5d45f46
beatrizacbs <beatrizacbs@gmail.com>
2017-07-31 17:22:36 -0300

    unique e not null

--- CodTXT.sql
+++ CodTXT.sql
@@ -1,27 +1,28 @@
-﻿create database HOPEatualizado;
-use HOPEatualizado;
+create database HOPE;
+use HOPE;
 
 create table instituicao(
-	id_doador varchar(5),
-    CNPJ varchar(14),
+	id_doador varchar(5) not null unique,
+    CNPJ varchar(14) not null unique,
     ramo_atuacao varchar(10),
     razao_social varchar(10),
-    email varchar(20),
+    email varchar(20) not null,
     nome_contato varchar(40),
-    data_entrada datetime,
-    telefone_1 varchar(10),
+    data_entrada datetime not null,
+    telefone_1 varchar(10)not null,
     telefone_2 varchar(10),
     num_doacoes int,
     nome_fantasia varchar(20),
-    tipo enum('DOADORA', 'RECEPTORA'),
+    tipo enum('DOADORA', 'RECEPTORA') not null,
     constraint intituicao_pk primary key (id_doador, CNPJ)
 );
 
 create table donativo_produto(
-	id_donativo varchar(10),
-    data_doacao datetime,
-    ja_em_estoque boolean,
-    id_instituicao_doadora varchar(19),
+	id_donativo varchar(10) not null,
+    data_doacao datetime not null,
+    ja_em_estoque boolean not null,
+    id_instituicao_doadora varchar(19) not null,
+    id_pf_doaora varchar(10) not null,
     constraint donativo_procuto_id_donativo primary key(id_donativo),
     constraint donativo_produto_id_instituaicao_doadora foreign key (id_instituicao_doadora) references instituicao(instituicao_pk),
     constraint donativo_produto_id_pf_doadora foreign key (id_pf_doadora) references pf_doadora(pf_doadora_pk)
@@ -29,19 +30,19 @@ create table donativo_produto(
 
 -- Falta fazer a relação N pra N com categoria_prod
 create table produto_ref(
-	cod_prod varchar (10),
-    descricao varchar(140),
-    cod_barra varchar(30),
+	cod_prod varchar (10) not null unique,
+    descricao varchar(140) not null,
+    cod_barra varchar(30) not null,
     marca varchar(10),
     constraint produto_ref_pk primary key (cod_prod)
 );
 
 create table pedido_produto(
-	id_pedido varchar(10),
-    data_pedido datetime, 
-    data_atendimento datetime,
-    status_pedido enum('ENTREGUE', 'NÃO ENTREGUE'),
-    id_receptora varchar(19),
+	id_pedido varchar(10)not null unique,
+    data_pedido datetime not null, 
+    data_atendimento datetime not null,
+    status_pedido enum('ENTREGUE', 'NÃO ENTREGUE') not null,
+    id_receptora varchar(19) not null,
     constraint pedido_produto_id_pedido primary key (id_pedido),
     constraint pedido_produto_id_receptora foreign key (id_receptora) references instituicao(instituicao_pk)
 );
@@ -50,229 +51,230 @@ create table pedido_produto(
 create table item_produto_pedido(
 	data_validade datetime,
     qte_pedida double,
-    num_seq varchar(5),
-    cod_prod varchar(10),
+    num_seq varchar(5) not null unique,
+    cod_prod varchar(10) not null,
     constraint item_produto_pedido_pk primary key (cod_prod, num_seq),
     constraint item_produto_pedido_cod_prod_fk foreign key (cod_prod) references produto_ref(produto_ref_pk)
 );
 
 create table item_produto_doado(
-	num_seq varchar(5),
-    data_entrada datetime,
-    data_validade datetime,
-    data_entrada_estoque datetime,
-    qte_doada double,
-    cod_prod varchar(19),
+	num_seq varchar(5) not null unique,
+    data_entrada datetime not null,
+    data_validade datetime not null,
+    data_entrada_estoque datetime not null,
+    qte_doada double not null,
+    cod_prod varchar(19) not null,
     constraint item_produto_doado_pk primary key (cod_prod, num_seq),
     constraint item_produto_doado_cod_prod_fk foreign key (cod_prod) references produto_ref(produto_ref_pk)
 );
 
 create table administrador(
-	data_inicio datetime,
-    nome varchar(20),
-    data_fim datetime,
-    CPF varchar(11),
+	data_inicio datetime not null,
+    nome varchar(20) not null,
+    data_fim datetime not null,
+    CPF varchar(11) not null unique,
     data_nascimento datetime,
     constraint administrador_pk primary key (CPF, data_inicio)
 );
 
 create table estoque(
-	codigo varchar(10),
-    descricao varchar(140),
-    data_ultima_entrada datetime,
-    cod_administrador varchar(20),
+	codigo varchar(10) not null unique,
+    descricao varchar(140) not null,
+    data_ultima_entrada datetime not null,
+    cod_administrador varchar(20) not null,
     constraint estoque_pk primary key (codigo),
     constraint estoque_administrador_fk foreign key (cod_administrador) references administrdor(administrador_pk)
 );
 
 create table item_prod_estoque(
-	qte_minima double, 
-    data_entrada datetime,
-    data_validade datetime,
-    qte_atual double,
-    num_seq varchar(5),
-    cod_prod varchar(10),
-    cod_estoque varchar(10),
+	qte_minima double not null, 
+    data_entrada datetime not null,
+    data_validade datetime not null,
+    qte_atual double not null,
+    num_seq varchar(5) not null unique,
+    cod_prod varchar(10) not null,
+    cod_estoque varchar(10) not null,
     constraint item_prod_estoque_pk primary key (cod_prod, num_seq),
     constraint item_prod_estoque_fk foreign key (cod_prod) references produto_ref(produto_ref_pk),
-    constraint item_prod_estoque_estoque_fk foreign key(codigo_estoque) references estoque(estoque_pk)
+    constraint item_prod_estoque_estoque_fk foreign key(cod_estoque) references estoque(estoque_pk)
 );
 
 create table entrega(
-	id_entrega varchar(10),
-    data_entrega datetime, 
-    status_entrega enum('ENTREGUE', 'NÃO ENTREGUE'),
-    hora_agendada time,
-    data_agendada datetime,
+	id_entrega varchar(10) not null unique,
+    data_entrega datetime not null, 
+    status_entrega enum('ENTREGUE', 'NÃO ENTREGUE') not null,
+    hora_agendada time not null,
+    data_agendada datetime not null,
     obs varchar(140),
-    id_voluntario varchar(10),
-    constraint entrega_pk primary key (ed_entrega),
+    id_voluntario varchar(10) not null,
+    constraint entrega_pk primary key (id_entrega),
     constraint entrega_id_voluntario foreign key (id_voluntario) references voluntario_com_veiculo(voluntario_com_veiculo_pk)
 );
 
 create table doadora(
-	data_ultima_doacao datetime
+	data_ultima_doacao datetime not null
 );
 
 create table receptora(
-	data_ultima_recepcao datetime,
-    cod_entrega varchar(10),
+	data_ultima_recepcao datetime not null,
+    cod_entrega varchar(10)not null,
     constraint receptora_cod_entrega_fk foreign key (cod_entrega) references entrega(id_entrega)
 );
 
 create table item_produto_entrega(
-	num_seq varchar(10),
-    qte_entregue double,
-    cod_entrega varchar(10),
-    cod_produto varchar(10),
-    constraint item_produto_entrega_cod_entrega foreign key (cod_entregue) references entrega(id_entrega),
+	num_seq varchar(10) not null unique,
+    qte_entregue double not null,
+    cod_entrega varchar(10) not null,
+    cod_produto varchar(10) not null,
+    constraint item_produto_entrega_cod_entrega foreign key (cod_entrega) references entrega(id_entrega),
     constraint item_produto_entrega_cod_produto foreign key (cod_produto) references produto_ref(cod_produto)
 );
 
 -- Falta fazer relacionamento N pra N com instituição
 create table endereco(
-	CEP varchar(8),
-    bairro varchar(15),
-    cidade varchar(10),
-    tipo_logradouro varchar(12),
-    nome varchar(10),
-    num_seq varchar(10),
-    estado varchar(15),
+	CEP varchar(8) not null,
+    bairro varchar(15) not null,
+    cidade varchar(10) not null,
+    tipo_logradouro varchar(12) not null,
+    nome varchar(10) not null,
+    num_seq varchar(10) not null unique,
+    estado varchar(15) not null,
     constraint endereco_pk primary key (num_seq)
 );
 
 -- Falta fazer o relacionamento N pra N com atividade
 create table pessoa_fisica(
-	CPF varchar(11),
-    primeiro_nome varchar(10),
+	CPF varchar(11) not null,
+    primeiro_nome varchar(10) not null,
     meio_nome varchar(10),
-    ultimo_nome varchar(10),
+    ultimo_nome varchar(10) not null,
     anonimato boolean,
     data_nasc datetime,
-    cep_endereco varchar(8),
+    cep_endereco varchar(8) not null,
     constraint pessoa_fisica_pk primary key (CPF),
     constraint pessoa_fisica_endereco foreign key (cep_endereco) references endereco(endereco_pk)    
 );
 
 create table pf_doadora(
-	cod_pf_doadora varchar(10),
+	cod_pf_doadora varchar(10) not null,
     constraint pf_doadora_fk foreign key (cod_pf_doadora) references pessoa_fisica(pessoa_fisica_pk)
 );
 
 -- Falta fazer o auto relacionamento dessa entidade
-create table categoria_prod(
-	id varchar(10),
-    obs varchar(140),
-    descricao varchar(200),
-    caracteristicas varchar(100),
-    constraint categoria_prod_pk primary key (id)
+create table categoria_produto(
+	id_categoria_produto varchar(10) not null unique,
+    obs_categoria_produto varchar(140),
+    descricao_categoria_produto varchar(200) not null,
+    caracteristicas_categoria_produto varchar(100) not null,
+    constraint categoria_produto_pk primary key (id_categoria_produto)
 );
 
-create table produto_ref_esta_em_categoria_prod(
-	cod_produto_ref varchar(10),
-    id_categoria_produto varchar(10),
-    constraint produto_ref_esta_em_categoria_prod_pk primary key (cod_produro_ref, id_categoria_produto),
+create table produto_ref_esta_em_categoria_produto(
+	cod_produto_ref varchar(10) not null,
+    id_categoria_produto varchar(10) not null,
+    constraint produto_ref_esta_em_categoria_prod_pk primary key (cod_produto_ref, id_categoria_produto),
     constraint cod_produto_ref_fk foreign key (cod_produto_ref) references produto_ref(produto_ref_pk),
     constraint id_categoria_produto_fk foreign key (id_categoria_produto) references categoria_prod(categoria_prod_pk)
 );
 
 create table atividade(
-	id varchar(10),
-    descricao varchar(100),
-    tipo varchar(20),
-    duracao_media varchar(10),
-    id_campanha varchar(10),
-    constraint atividade_pk primary key (id),
+	id_atividade varchar(10) not null unique,
+    descricao varchar(100) not null,
+    tipo varchar(20) not null,
+    duracao_media varchar(10) not null,
+    id_campanha varchar(10) not null,
+    constraint atividade_pk primary key (id_atividade),
     constraint atividade_id_campanha_fk foreign key (id_campanha) references campanha(campanha_pk)
 );
 
 create table campanha(
-	id_campanha varchar(10),
-    data_inicio datetime,
-    data_fim datetime,
-    nome varchar(20),
-    qtd_vagas int,
-    id_receptora varchar(20),
+	id_campanha varchar(10) not null unique,
+    data_inicio datetime not null,
+    data_fim datetime not null,
+    nome varchar(20) not null,
+    qtd_vagas int not null,
+    id_receptora varchar(20) not null,
     constraint campanha_pk primary key (id_campanha),
     constraint campanha_id_receptora foreign key (id_receptora) references instituicao(instituicao_pk)
 );
 
 create table atividade_atribuida_pessoa_fisica(
-	id_atividade varchar(10),
-    id_pessoa_fisica varchar(10),
+	id_atividade varchar(10) not null,
+    id_pessoa_fisica varchar(10) not null,
     constraint atividade_atribuida_pessoa_fisica_pk primary key(id_pessoa_fisica, id_atividade),
     constraint atividade_atribuida_pessoa_fisica_id_atividade foreign key (id_atividade) references atividade(atividade_pk),
     constraint atividade_atribuida_pessoa_fisica_id_pessoa_fisica foreign key (id_pessoa_fisica) references pessoa_fisica(pessoa_fisica_pk)
 );
 
 create table voluntario(
-    data_inicio datetime,
-    data_fim datetime,
-    id_voluntario varchar(10),
+    data_inicio datetime not null,
+    data_fim datetime not null,
+    id_voluntario varchar(10) not null unique,
     constraint voluntario_pk primary key (id_voluntario, data_inicio)
 );
 
 create table voluntario_com_veiculo(
-    placa_veiculo varchar(8),
-    marca_veiculo varchar(10),
-    id_voluntario varchar(10),
-    constraint voluntario_com_veiculo_pk primary key (id_voluntario, placa_veiculo)
+    placa_veiculo varchar(8) not null,
+    marca_veiculo varchar(10) not null,
+    id_voluntario varchar(10) not null,
+    constraint voluntario_com_veiculo_pk primary key (id_voluntario, placa_veiculo),
+    constraint voluntario_com_veiculo_id_voluntario_fk foreign key (id_voluntario) references voluntario(voluntario_pk)
 );
 
 create table voluntario_sem_veiculo(
-    id_voluntario varchar(10),
+    id_voluntario varchar(10) not null,
     constraint voluntario_sem_veiculo_pk primary key (id_voluntario)
 );
 
 create table instituicao_tem_endereco(
-    complemento_1 varchar(15),
-    complemento_2 varchar(15),
-    numero varchar(5),
-    id_instituicao varchar(10),
-    cep_endereco varchar(8),
+    complemento_1 varchar(15) not null,
+    complemento_2 varchar(15) not null,
+    numero varchar(5) not null,
+    id_instituicao varchar(10) not null,
+    cep_endereco varchar(8) not null,
     constraint instituicao_tem_endereco_pk primary key (id_instituicao, cep_endereco),
     constraint instituicao_tem_endereco_id_instituicao foreign key (id_instituicao) references instituicao(intituicao_pk),
     constraint instituicao_tem_endereco_cep_endereco foreign key (cep_endereco) references endereco(endereco_pk)
 );
 
 create table horario(
-    confirmacao_dia boolean,
-    hora_inicio datetime,
-    periodo_dia enum('MANHÃ', 'TARDE', 'NOITE'),
-    num_seq varchar(10),
-    hora_fim datetime,
-    ind_feriado varhcar(10), -- queria saber o que é isso
-    dia_semana enum('SEGUNDA', 'TERÇA', 'QUARTA', 'QUINTA', 'SEXTA', 'SABADO', 'DOMINGO'),
+    confirmacao_dia boolean not null,
+    hora_inicio datetime not null,
+    periodo_dia enum('MANHÃ', 'TARDE', 'NOITE') not null,
+    num_seq varchar(10) not null unique,
+    hora_fim datetime not null,
+    ind_feriado varchar(10) not null, -- queria saber o que é isso
+    dia_semana enum('SEGUNDA', 'TERÇA', 'QUARTA', 'QUINTA', 'SEXTA', 'SABADO', 'DOMINGO') not null,
     constraint horario_pk primary key (num_seq)
 );
 
 create table voluntario_possui_horario(
-    id_voluntario varchar(10),
-    horario_num_seq varchar(10),
+    id_voluntario varchar(10) not null,
+    horario_num_seq varchar(10) not null,
     constraint voluntario_possui_horario_pk primary key (id_voluntario, horario_num_seq),
     constraint voluntario_possui_horario_id_voluntario_fk foreign key (id_voluntario) references voluntario(voluntario_pk),
     constraint voluntario_possui_horario_num_seq_fk foreign key (horario_num_seq) references horario(horario_pk)
 );
 
 create table banco(
-    razao_social varchar(10),
-    CNPJ varchar(14),
-    nome varchar(30),
-    codigo_banco varchar(10),
-    cep_endereco varchar(8),
-    numero varchar(5),
+    razao_social varchar(10) not null,
+    CNPJ varchar(14) not null unique,
+    nome varchar(30) not null,
+    codigo_banco varchar(10) not null,
+    cep_endereco varchar(8) not null,
+    numero varchar(5) not null,
     constraint banco_pk primary key (codigo_banco),
     constraint banco_cep_endereco_fk foreign key (cep_endereco) references endereco(endereco_pk)
 );
 
 create table transferencia(
-    agencia_destino varchar(10),
-    conta_origem varchar(10),
-    conta_destino varchar(10),
-    agencia_origem varchar(10),
-    transferencia_destino_banco varchar(10),
-    transferencia_origem_banco varchar(10),
-    codigo_transferencia varchar(10),
+    agencia_destino varchar(10) not null,
+    conta_origem varchar(10) not null,
+    conta_destino varchar(10) not null,
+    agencia_origem varchar(10) not null,
+    transferencia_destino_banco varchar(10) not null,
+    transferencia_origem_banco varchar(10) not null,
+    codigo_transferencia varchar(10) not null,
     constraint transferencia_pk primary key (codigo_transferencia),
     constraint transferencia_origem_banco_fk foreign key (transferencia_origem_banco) references banco(banco_pk),
     constraint transferencia_destino_banco_fk foreign key (transferencia_destino_banco) references banco(banco_pk),
@@ -280,33 +282,32 @@ create table transferencia(
 );
 
 create table cartao(
-    cod_seguranca varchar(3),
-    validade datetime, 
-    bandeira varchar(9),
-    numero varchar(20),
-    id_transacao varhcar(10),
-    constraint cartao_pk primary key 
+    cod_seguranca varchar(3) not null,
+    validade datetime not null, 
+    bandeira varchar(9) not null,
+    numero varchar(20) not null unique,
+    id_transacao varchar(10)
 );
 
 create table donativo_financeiro(
-    id_financeiro varchar(4),
-    data_doacao datetime,
-    id_instituicao_doadora varchar(10),
+    id_financeiro varchar(4) not null,
+    data_doacao datetime not null,
+    id_instituicao_doadora varchar(10) not null,
     constraint id_instituicao_doadora_fk foreign key (id_instituicao_doadora) references instituicao(instituicao_pk)
     
 );
 
 create table comprovante_doacao(
-	num_doc varchar(10),
-    descricao varchar(30),
-    arquivo_doc varchar(10),
+	num_doc varchar(10) not null unique,
+    descricao varchar(30) not null,
+    arquivo_doc varchar(10) not null,
     constraint num_doc_pk primary key (num_doc)
 );
 
 create table doc_protocolo(
-	num_protocolo varchar (10),
-    arquivo varchar(10),
-    descricao varchar(30),
+	num_protocolo varchar(10) not null unique,
+    arquivo varchar(10) not null,
+    descricao varchar(30) not null,
     constraint num_protocolo_pk primary key (num_protocolo)
 
 );
